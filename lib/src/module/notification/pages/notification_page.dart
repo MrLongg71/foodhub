76f49cbbdd367d4001b066f61787acdcf20f9fb7
mrlongg71 <mrlongg71@gmail.com>
2021-05-20 23:12:45 +0700

    init notification page

--- lib/src/module/notification/pages/notification_page.dart
+++ lib/src/module/notification/pages/notification_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:foodhub/src/util/styles.dart';
+import 'package:foodhub/src/util/uidata.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({Key key}) : super(key: key);
@@ -10,6 +12,63 @@ class NotificationPage extends StatefulWidget {
 class NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: _buildAppbar(),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(child: _buildListNotify());
+  }
+
+  Widget _buildAppbar() {
+    return AppBar(
+      elevation: 0,
+      title: Text("Notification"),
+    );
+  }
+
+  Widget _buildListNotify() {
+    return ListView.builder(
+        itemCount: 100,
+        itemBuilder: (ctx, index) {
+          return _buildItemNotify();
+        });
+  }
+
+  Widget _buildItemNotify() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                UIData.image1,
+                width: 60,
+                height: 60,
+                fit: BoxFit.cover,
+              )),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: RichText(
+              text: TextSpan(
+                text: 'Đặt món thành công \n',
+                style: StylesText.body15LeftW500White,
+                children: <TextSpan>[
+                  TextSpan(
+                      text: '12 tháng 6,2020',
+                      style: StylesText.body14SemiBoldGrey),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
